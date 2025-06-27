256a68f7b0f84ebe5f224fbf0da24ea35455540d
Alejandro S <jorale2133@gmail.com>
2025-06-26 22:21:52 -0600

    se puede mover motor 1 y 2

--- Software/Robot_Controller/Robot_controller/Robot_controller.ino
+++ Software/Robot_Controller/Robot_controller/Robot_controller.ino
@@ -1,6 +1,9 @@
 #include <ESP32Servo.h>
 
-static const int servoPin1 = 25;
+static const int servoPin1 = 25; //Pin Servo 1
+static const int servoPin2 = 33; //Pin Servo 2
+static const int servoPin3 = 32; //Pin servo 3
+static const int servoPin4 = 34; //Pin Servo 4
 
 int Vrx1 = A15; // input pin for joystick1 X
 int Vry1 = A16; // input pin for joystick1 Y
@@ -16,8 +19,15 @@ JoyStick stick1;
 JoyStick stick2;
 
 Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
 
-int dx1 = 90;
+int dx1 = 90; //1<dx1<180
+int dx2 = 20;
+int dx3 = 0;
+int dx4 = 90;
+
 void setup() {
   Serial.begin(115200);
   stick1.vrx = 0;
@@ -26,6 +36,9 @@ void setup() {
   stick2.vry = 0;
 
   servo1.attach(servoPin1);
+  servo2.attach(servoPin2);
+  servo3.attach(servoPin3);
+  servo4.attach(servoPin4);
 }
 
 void loop() {
@@ -35,6 +48,7 @@ void loop() {
   stick2.vry = analogRead(Vry2);
 
   moveServoMotor1(stick1.vrx);
+  moveServoMotor2(stick1.vry);
 
 /*
   Serial.print("Stick 1 - X: ");
@@ -47,7 +61,7 @@ void loop() {
   Serial.print(" | Y: ");
   Serial.println(stick2.vry);
 */
-  delay(20);
+  delay(30);
 }
 
 void moveServoMotor1(int x1){
@@ -58,14 +72,29 @@ void moveServoMotor1(int x1){
         dx1--;
     }
 
-
-
-    if(x1 == 4095 && dx1 >0 && dx1 < 180){
+    if(x1 == 4095 && dx1 > 0 && dx1 < 180){
       dx1--;
       if(dx1==0)
         dx1++;  
-    }
-    
+    }   
     servo1.write(dx1);
+    Serial.print("Dx1: ");
     Serial.println(dx1);
+}
+
+void moveServoMotor2(int x2){
+     if(x2 == 4095 && dx2 > 0 && dx2 < 180){
+      dx2++;
+      if(dx2==180)
+        dx2--;
+    }
+
+    if(x2 == 0 && dx2 > 0 && dx2 < 180){
+      dx2--;
+      if(dx2==0)
+        dx2++;  
+    }   
+    servo2.write(dx2);
+    Serial.print("Dx2: ");
+    Serial.println(dx2); 
 }
